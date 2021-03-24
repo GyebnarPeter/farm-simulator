5d6fc05570254d877cc963fcdc5d550e99fa4baa
GyebnarPeter <gyebnarpetter@gmail.com>
2021-03-24 15:34:58 +0100

    complete task

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,42 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+import com.codecool.farm.animal.Cattle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
+
+    List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals() {
+        animals.forEach(Animal::feed);
+    }
+
+    public void butcher(Butcher butcher) {
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> statusList = new ArrayList<>();
+        for (Animal animal : animals) {
+            String animalType = animal instanceof Cattle ? "cattle" : "pig";
+            statusList.add("There is a " + animal.getSize() + " sized " + animalType + " in the farm.");
+        }
+        return statusList;
+    }
 
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,16 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    private int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size += size;
+    }
+
+    public abstract void feed();
+
 }
